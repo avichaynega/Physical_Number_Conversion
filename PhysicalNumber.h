cb028623af572bae27f53d7de4d70dec0a6e9f50
OrenZauda <45006271+OrenZauda@users.noreply.github.com>
2019-04-12 13:02:22 +0300

    Update PhysicalNumber.h

--- PhysicalNumber.h
+++ PhysicalNumber.h
@@ -21,15 +21,16 @@ class PhysicalNumber {
     const PhysicalNumber operator+()const;
     const PhysicalNumber operator-()const;
     
-    
+    //overload simple add and substruct operators
     const PhysicalNumber operator+(const PhysicalNumber& other)const;
     const PhysicalNumber operator-(const PhysicalNumber& other) const;
 
-
+    //overload simple add and substruct operators, witch the result stores in the first number
     PhysicalNumber operator+=(const PhysicalNumber& other) ;
     PhysicalNumber operator-=(const PhysicalNumber& other) ;
-
+    //overload not operator
     bool operator!() const;
+    //overload comerasion operators
     bool operator<(const PhysicalNumber& p) const;
     bool operator>(const PhysicalNumber& p)const;
     bool operator<=(const PhysicalNumber& p);     	
@@ -38,17 +39,20 @@ class PhysicalNumber {
 
 
     
-
+    //overload operator ++ which increasing number by 0ne
     PhysicalNumber operator++();
+    //overload operator ++ which increasing number by 0ne with parameter
     PhysicalNumber operator++(int dummy_flag_for_postfix_increment) ;
+    //overload operator ++ which decreasing number by 0ne
     PhysicalNumber operator--();
+     //overload operator ++ which dencreasing number by 0ne with parameter
     PhysicalNumber operator--(int dummy_flag_for_postfix_increment) ;
     
 
     
     //friend bool operator==(const PhysicalNumber& c1, const PhysicalNumber& c2);
    friend bool operator!=(const PhysicalNumber& p1,const PhysicalNumber& p2);
-    
+    // input and output operators
     friend ostream& operator<< (ostream& os, const PhysicalNumber& c);
    friend istream& operator>> (istream& is, PhysicalNumber& c);
     
